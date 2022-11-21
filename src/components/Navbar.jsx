4e29ea7daa16952c690adf85d8d21fdd2fafd030
Osman Rasooli <mohammad.osmanrasooli1973@gmail.com>
2022-11-22 01:14:50 +0430

    working on Navbar Popups

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ import { useStateContext } from "../contexts/ContextProvider";
 
 const NavButton = ({ customFunc, icon, title, color, dotColor }) => {
   return (
-    <TooltipComponent title="Menu" position="TopCenter">
+    <TooltipComponent content={title} position="BottomCenter">
       <button
         type="button"
         onClick={customFunc}
@@ -25,17 +25,18 @@ const NavButton = ({ customFunc, icon, title, color, dotColor }) => {
         >
           {icon}
         </span>
-        {title}
       </button>
     </TooltipComponent>
   );
 };
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, isClicked, setIsClicked } =
+    useStateContext();
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
+        title="Menu"
         customFunc={() => setActiveMenu((prev) => !prev)}
         color="blue"
         icon={<AiOutlineMenu />}
@@ -47,6 +48,34 @@ const Navbar = () => {
           color="blue"
           icon={<FiShoppingCart />}
         />
+        <NavButton
+          title="Chat"
+          customFunc={() => {}}
+          color="blue"
+          dotColor="#03C9D7"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          customFunc={() => {}}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => {}}
+          >
+            <img src={avatar} className="rounded-full w-8 h-8 " />
+            <p>
+              <span className="text-gray-400 text-14">Hi, </span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
       </div>
     </div>
   );
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,8 +11,11 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsCliked] = useState(initialState);
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{ activeMenu, setActiveMenu, isClicked, setIsCliked }}
+    >
       {children}
     </StateContext.Provider>
   );
