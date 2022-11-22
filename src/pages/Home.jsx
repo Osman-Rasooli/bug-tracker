7dd0657eb5212f4dc1c111baf56fbb2b0114c931
Osman Rasooli <mohammad.osmanrasooli1973@gmail.com>
2022-11-23 02:20:11 +0430

    working on homepage cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,39 @@
 import React from "react";
+import { RiSuitcaseFill } from "react-icons/ri";
+import { BsPersonBadgeFill, BsCalendar2DateFill } from "react-icons/bs";
+import { AiTwotoneBug } from "react-icons/ai";
+
 const Home = () => {
   return (
     <div>
       <header>
-        <p className="px-8 py-6 border-t-1 text-xl font-bold tracking-wider text-gray-500">
+        <p className="px-12 py-6 border-t-1 text-xl font-bold tracking-wider text-gray-500">
           Home
         </p>
       </header>
       <main>
-        <div className="overview">
-          <div>
-            <span>10</span>
+        <div className="overview px-10 flex flex-wrap gap-8">
+          <div className="home-card card1 rounded-md p-5 flex-1 h-22 relative bg-red-400">
+            <span className=" text-3xl font-bold">10</span>
             <p>Active Projects</p>
+            <RiSuitcaseFill className="absolute text-5xl -rotate-12 top-6 right-6" />
           </div>
-          <div>
-            <span>25</span>
+          <div className="home-card card2 rounded-md p-5 flex-1 h-22 relative bg-red-300">
+            <span className=" text-3xl font-bold">25</span>
             <p>Working Developer</p>
+            <BsPersonBadgeFill className="absolute text-5xl -rotate-12 top-6 right-6" />
           </div>
-          <div>
-            <span>108</span>
-            <p>Open Bugs</p>
+          <div className="home-card card3 rounded-md p-5 flex-1 h-22 relative bg-red-200">
+            <span className=" text-3xl font-bold">108</span>
+            <p>
+              Open <abbr title="Bugs, Issues, Improvements">Tickets</abbr>
+            </p>
+            <AiTwotoneBug className="absolute text-5xl -rotate-12 top-6 right-6" />
           </div>
-          <div>
-            <span>150</span>
-            <p>Open Issues</p>
-          </div>
-          <div>
-            <span>15</span>
-            <p>Open Improvements</p>
-          </div>
-          <div>
-            <span>15</span>
+          <div className="home-card card4 rounded-md p-5 flex-1 h-22 relative bg-red-800">
+            <span className=" text-3xl font-bold">35</span>
             <p>Due in 1 Month</p>
+            <BsCalendar2DateFill className="absolute text-5xl -rotate-12 top-6 right-6" />
           </div>
         </div>
       </main>
